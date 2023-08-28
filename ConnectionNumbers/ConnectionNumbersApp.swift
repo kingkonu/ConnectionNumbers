4721e9055b8169468beba1ce7650eecb9cc85913
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-08-28 23:44:04 +0300

    Initial Commit

---
+++ ConnectionNumbers/ConnectionNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionNumbersApp.swift
+//  ConnectionNumbers
+//
+//  Created by Alexey Kanaev on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ConnectionNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConnectionNumbers/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ConnectionNumbers
+//
+//  Created by Alexey Kanaev on 8/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
